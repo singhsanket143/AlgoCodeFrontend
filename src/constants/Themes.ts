f36b07abd52b2154116454068279f8791093bd45
Abhishek Singh <81167517+digiabhi@users.noreply.github.com>
2024-03-10 16:13:07 +0530

    FEAT:- Added support for theme change in ace editor

---
+++ src/constants/Themes.ts
@@ -0,0 +1,35 @@
+export default [
+    {
+        themeName: 'Monokai', value: 'monokai',   
+    },
+    {
+        themeName: 'Github', value: 'github',
+    },
+    {
+        themeName: 'Github Dark', value: 'github_dark',
+    },
+    {
+        themeName: 'Tomorrow', value: 'tomorrow',
+    },
+    {
+        themeName: 'Kuroir', value: 'kuroir',
+    },
+    {
+        themeName: 'Twilight', value: 'twilight',   
+    },
+    {
+        themeName: 'Xcode', value: 'xcode',
+    },
+    {
+        themeName: 'Textmate', value: 'textmate',
+    },
+    {
+        themeName: 'Solarized Dark', value: 'solarized_dark',
+    },
+    {
+        themeName: 'Solarized Light', value: 'solarized_light',
+    },
+    {
+        themeName: 'Terminal', value: 'terminal',
+    },
+]

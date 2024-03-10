776f7281fd91e87ba5bf7b1fcd366601135c3e7c
Sanket Singh <singhsanket143@gmail.com>
2024-03-10 14:39:25 +0530

    integrated sidebar as well

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import SideBar from './components/SideBar';
 
 import ProblemDescription from './pages/Description/ProblemDescription';
 
@@ -16,6 +17,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <SideBar />
       <ProblemDescription descriptionText={markdownText}></ProblemDescription>
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,13 @@ function Navbar() {
         <div className="navbar bg-base-100 border-b-2">
             <div className="navbar-start">
                 <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                </div>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Homepage</a></li>
-                    <li><a>Portfolio</a></li>
-                    <li><a>About</a></li>
-                </ul>
+                <label htmlFor="my-drawer">
+                    <div role="button" className="btn btn-ghost btn-circle">
+                        
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                        
+                    </div>
+                </label>
                 </div>
             </div>
             <div className="navbar-center">

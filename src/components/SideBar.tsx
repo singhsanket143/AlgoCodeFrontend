cce01690e5cb577eca0305cc60f8702133fcc82c
Sanket Singh <singhsanket143@gmail.com>
2024-03-17 15:05:33 +0530

    Fix: Fix for sidebar overlap added

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,6 @@
 function SideBar() {
     return (
-        <div className="drawer">
+        <div className="drawer z-10">
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
             
             <div className="drawer-side">

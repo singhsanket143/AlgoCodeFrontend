c64db47e1b39cf8014b8c74f3a813e61ea25c8fb
Sourav Sinha <4souravsinhain@gmail.com>
2024-03-17 20:48:18 +0530

    Fix: height adjustment
    - gave navbar a particular height
    - adjusted the height of problem description accordingly.

--- src/pages/Description/ProblemDescription.tsx
+++ src/pages/Description/ProblemDescription.tsx
@@ -89,7 +89,7 @@ function Description({ descriptionText }: {descriptionText: string}) {
 
     return (
         <div 
-            className='flex w-screen h-full'
+            className='flex w-screen h-[calc(100vh-57px)]'
             onMouseMove={onDrag}
             onMouseUp={stopDragging}
             

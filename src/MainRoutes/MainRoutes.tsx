0db4899d968cab9f83575664290d0a68bebbb110
rohan <rohanthakur89768@gmail.com>
2024-03-22 07:53:39 +0530

    added router for other pages as well

--- src/MainRoutes/MainRoutes.tsx
+++ src/MainRoutes/MainRoutes.tsx
@@ -9,8 +9,7 @@ export default function MainRoutes(){
     const markdownText = SampleProblem1.problemStatement;
     return(
         <Routes>
-            {/* <Route path="/" element={<Navbar/>}></Route> */}
-            <Route  path="/" element={<ProblemDescription   descriptionText={markdownText}/>}></Route>
+            <Route  path="/" element={<ProblemDescription  descriptionText={markdownText}/>}></Route>
             <Route path="/signup" element={<Signup/>}  ></Route>
             <Route path="/signin" element={<Signin/>}></Route>
         </Routes>

fd9e1b8b0bf2d0b5a24254b004cb83ea02a59262
Abhishek Singh <singhabhishek111213@gmail.com>
2024-04-10 17:46:16 +0530

    refactor: streamlined acebuild imports to single file

---
+++ src/imports/AceBuildImports.ts
@@ -0,0 +1,43 @@
+// Import all themes
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-github_dark";
+import "ace-builds/src-noconflict/theme-tomorrow";
+import "ace-builds/src-noconflict/theme-kuroir";
+import "ace-builds/src-noconflict/theme-twilight";
+import "ace-builds/src-noconflict/theme-xcode";
+import "ace-builds/src-noconflict/theme-textmate";
+import "ace-builds/src-noconflict/theme-solarized_dark";
+import "ace-builds/src-noconflict/theme-solarized_light";
+import "ace-builds/src-noconflict/theme-terminal";
+
+// Import Ace
+import 'ace-builds/src-noconflict/ace';
+
+// Import all modes
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-python";
+
+// Import language tools
+import "ace-builds/src-noconflict/ext-language_tools";
+
+// Re-export everything
+export * from "ace-builds/src-noconflict/theme-monokai";
+export * from "ace-builds/src-noconflict/theme-github";
+export * from "ace-builds/src-noconflict/theme-github_dark";
+export * from "ace-builds/src-noconflict/theme-tomorrow";
+export * from "ace-builds/src-noconflict/theme-kuroir";
+export * from "ace-builds/src-noconflict/theme-twilight";
+export * from "ace-builds/src-noconflict/theme-xcode";
+export * from "ace-builds/src-noconflict/theme-textmate";
+export * from "ace-builds/src-noconflict/theme-solarized_dark";
+export * from "ace-builds/src-noconflict/theme-solarized_light";
+export * from "ace-builds/src-noconflict/theme-terminal";
+export * from 'ace-builds/src-noconflict/ace';
+export * from "ace-builds/src-noconflict/mode-javascript";
+export * from "ace-builds/src-noconflict/mode-c_cpp";
+export * from "ace-builds/src-noconflict/mode-java";
+export * from "ace-builds/src-noconflict/mode-python";
+export * from "ace-builds/src-noconflict/ext-language_tools";
